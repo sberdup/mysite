a49b16e86ea3016283e2de9208ef56ac838073bf
Sergio Berdeja <sergioberdeja@gmail.com>
2023-03-20 18:36:36 -0600

    update html

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <title>My Portfolio</title>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <header>
+          <h1>My Portfolio</h1>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section>
+            <h2>About Me</h2>
+            <p>Write a brief introduction about yourself.</p>
+          </section>
+          <section>
+            <h2>Portfolio</h2>
+            <div className="grid">
+              <div className="item">
+                <a href="#">
+                  <img src="https://via.placeholder.com/400x300" alt="Project Name" />
+                  <h3>Project Name</h3>
+                </a>
+              </div>
+              <div className="item">
+                <a href="#">
+                  <img src="https://via.placeholder.com/400x300" alt="Project Name" />
+                  <h3>Project Name</h3>
+                </a>
+              </div>
+              <div className="item">
+                <a href="#">
+                  <img src="https://via.placeholder.com/400x300" alt="Project Name" />
+                  <h3>Project Name</h3>
+                </a>
+              </div>
+            </div>
+          </section>
+        </main>
+        <footer>
+          <p>&copy; 2023 My Portfolio</p>
+        </footer>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
